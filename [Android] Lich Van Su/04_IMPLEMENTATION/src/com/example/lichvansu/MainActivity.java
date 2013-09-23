b601d8b5ec50519e92628544d0aa0b2d16d0e36c
Duy Trần <duytran.bkict@gmailc.om>
2013-09-23 16:12:00 +0700

    Implement basic function:

--- [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/MainActivity.java
+++ [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lichvansu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void btn_xem_ngay_onClick(View view) {
+		Intent intent = new Intent(this, XemNgayActivity.class);
+		startActivity(intent);
 	}
 
 }
--- [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/SplashScreenActivity.java
+++ [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/SplashScreenActivity.java
@@ -1,22 +1,30 @@
 package com.example.lichvansu;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends Activity {
+
+	// Splash screen timer
+	private static int SPLASH_TIME_OUT = 2000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash_screen, menu);
-		return true;
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+				startActivity(i);
+
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
 	}
 
 }
