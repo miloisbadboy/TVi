43043544ec416331f8380b7d30a234748018d4de
Duy Trần <duytran.bkict@gmailc.om>
2013-09-29 12:09:13 +0700

    Add swipe gesture detector

--- [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/main/MainActivity.java
+++ [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/main/MainActivity.java
@@ -8,18 +8,26 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lichvansu.R;
-import com.example.lichvansu.XemNgayFragment;
 import com.example.lichvansu.xemngay.XemNgayActivity;
+import com.example.lichvansu.xemngay.XemNgayFragment;
 
-public class MainActivity extends FragmentActivity implements TabListener {
+public class MainActivity extends FragmentActivity implements TabListener,
+		OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,17 @@ public class MainActivity extends FragmentActivity implements TabListener {
 				.setTabListener((TabListener) this));
 		actionBar.addTab(actionBar.newTab().setText("Đổi Ngày")
 				.setTabListener((TabListener) this));
+
+		FrameLayout a = (FrameLayout) findViewById(R.id.container);
+		gestureDetector = new GestureDetector(new MyGestureDetector());
+		gestureListener = new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		};
+
+		a.setOnClickListener(this);
+		a.setOnTouchListener(gestureListener);
 	}
 
 	@Override
@@ -92,6 +111,44 @@ public class MainActivity extends FragmentActivity implements TabListener {
 					ARG_SECTION_NUMBER)));
 			return textView;
 		}
+	}
+
+	private static final int SWIPE_MIN_DISTANCE = 10;
+	private static final int SWIPE_MAX_OFF_PATH = 100;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+	private GestureDetector gestureDetector;
+	View.OnTouchListener gestureListener;
+
+	class MyGestureDetector extends SimpleOnGestureListener {
+		@Override
+	    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			Log.w("Debug", "(" + e1.getX() + "; " + e1.getY() + ") ; (" + e2.getX() + "; " + e2.getY() + ")");
+	        try {
+	        	if (Math.abs(e1.getY() - e2.getY()) > Math.abs(e1.getX() - e2.getX())) {
+	        		// downward swipe
+	        		if (e2.getY() - e1.getY() > SWIPE_MAX_OFF_PATH && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
+		                Toast.makeText(MainActivity.this, "Downward Swipe", Toast.LENGTH_SHORT).show();
+		            else if (e1.getY() - e2.getY() > SWIPE_MAX_OFF_PATH && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY)
+		                Toast.makeText(MainActivity.this, "Upward Swipe", Toast.LENGTH_SHORT).show();	
+	        	} else {
+	        		// right to left swipe
+		            if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+		                Toast.makeText(MainActivity.this, "Left Swipe", Toast.LENGTH_SHORT).show();
+		            }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+		                Toast.makeText(MainActivity.this, "Right Swipe", Toast.LENGTH_SHORT).show();
+		            }        		
+	        	}
+	        } catch (Exception e) {
+	            // nothing
+	        }
+	        return false;
+	    }
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
