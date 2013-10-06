1aafc843da701c857f8c049578a8678e7a4edfbf
Duy Trần <duytran.bkict@gmailc.om>
2013-10-06 18:32:15 +0700

    Update

--- [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/monthcalendar/MonthCalendarFragment.java
+++ [Android] Lich Van Su/04_IMPLEMENTATION/src/com/example/lichvansu/monthcalendar/MonthCalendarFragment.java
@@ -1,12 +1,16 @@
 package com.example.lichvansu.monthcalendar;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.example.lichvansu.R;
+import com.example.lichvansu.datecalendar.DateCalendarElementFragment;
 
 public class MonthCalendarFragment extends Fragment {
 
@@ -14,7 +18,26 @@ public class MonthCalendarFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_month_calendar, container,
-				false);
+		Bundle args = getArguments();
+		View view = inflater.inflate(R.layout.fragment_month_calendar,
+				container, false);
+
+		getChildFragmentManager()
+				.beginTransaction()
+				.add(R.id.element_container,
+						getElementFragment(Calendar.getInstance())).commit();;
+
+		return view;
+	}
+
+	private Fragment getElementFragment(Calendar date) {
+		Fragment frag = new MonthCalendarElementFragment();
+		Bundle args = new Bundle();
+		Calendar currentDate = Calendar.getInstance();
+		args.putInt("month", currentDate.get(Calendar.MONTH));
+		args.putInt("year", currentDate.get(Calendar.YEAR));
+		frag.setArguments(args);
+
+		return frag;
 	}
 }
